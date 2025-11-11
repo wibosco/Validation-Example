a01493bb13bea043f5375b24b14e0db3ec053d50
William Boles <wibosco@users.noreply.github.com>
2025-11-11 15:55:04 +0000

    Improved comments

--- Validation-Example/Features/Registration/Validation/Validators/Custom/PasswordValidator.swift
+++ Validation-Example/Features/Registration/Validation/Validators/Custom/PasswordValidator.swift
@@ -11,35 +11,34 @@ struct PasswordValidator: Validator {
     // MARK: - Validator
     
     func validate(_ value: String) throws(PasswordValidationError) {
-        // 1. Too short
+        // 1. Must have at least 8 characters
         guard value.count >= 8 else {
             throw .tooShort
         }
         
-        // 2. Too long
+        // 2. Must have at most 24 characters
         guard value.count <= 24 else {
             throw .tooLong
         }
         
-        // 3. Missing at least one lowercase character
+        // 3. Must have at least one lowercase character
         guard value.contains(where: { $0.isLowercase }) else {
             throw .missingLowercase
         }
         
-        // 4. Missing at least one uppercase character
+        // 4. Must have at least one uppercase character
         guard value.contains(where: { $0.isUppercase }) else {
             throw .missingUppercase
         }
         
-        // 5. Missing at least one number
+        // 5. Must have at least one number
         guard value.contains(where: { $0.isNumber }) else {
             throw .missingNumber
         }
         
-        // 6. Missing at east one special character
+        // 6. Must have at least one special character
         let allowedSpecialChars = CharacterSet(charactersIn: "&,_,-,@")
         let hasSpecialChar = value.unicodeScalars.contains { allowedSpecialChars.contains($0) }
-        
         guard hasSpecialChar else {
             throw .missingSpecialCharacter
         }
